054ff833f6270888fb550c43306f345c361c14b7
r.tatarinov <r.tatarinov@optimism.ru>
2023-12-29 14:57:14 +0300

    first commit

---
+++ frontend/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+          <h1>React-приложение</h1>
+          <p>Добро пожаловать!</p>
+        </div>
+    );
+}
+
+export default App;
